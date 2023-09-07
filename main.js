5ee918513decabfa015f4c483576a3db6cf37f60
서유정 <azureapple1018@gmail.com>
2023-09-07 18:49:15 +0900

    Create : Weather App

---
+++ main.js
@@ -0,0 +1,35 @@
+let weather = {
+    apikey: 'bed6ed2482ff99e240cdb9c90e9c7e7b',
+    fetchWeather(city) {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apikey}`)
+            .then((res) => res.json())
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+
+        document.getElementById('city').innerText = `Weather in ${name}`;
+        document.getElementById('temp').innerText = `${parseInt(temp)}°C`;
+        document.getElementById('icon').src = `https://openweathermap.org/img/wn/${icon}.png`;
+        document.getElementById('description').innerText = description;
+        document.getElementById('humidity').innerText = `Humidity: ${humidity}%`;
+        document.getElementById('wind').innerText = `Wind speed: ${Math.floor(speed)}m/s`;
+    },
+};
+
+const search = () => {
+    const input = document.getElementById('input');
+    const body = document.getElementById('body');
+
+    weather.fetchWeather(input.value);
+    body.style.backgroundImage = `url('https://source.unsplash.com/1920x1080/?${input.value}')`;
+};
+
+const enterKey = (e) => {
+    if (e.code == 'Enter') {
+        search();
+    }
+};
